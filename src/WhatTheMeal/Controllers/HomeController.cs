3f8b02bc3ea95486fa9bdde7459568eaf3c0faa0
Wassim AZIRAR <wassim.azirar@gmail.com>
2016-03-16 23:33:44 +0100

    Added MVC and Bower

---
+++ src/WhatTheMeal/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace WhatTheMeal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
